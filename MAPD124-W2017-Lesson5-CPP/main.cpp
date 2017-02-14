f3884429ac4071e21847bc29aa68da1705cdd7fb
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-14 18:56:16 -0500

    Initial Commit

---
+++ MAPD124-W2017-Lesson5-CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MAPD124-W2017-Lesson5-CPP
+//
+//  Created by Tom Tsiliopoulos on 2017-02-14.
+//  Copyright © 2017 Tom Tsiliopoulos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
